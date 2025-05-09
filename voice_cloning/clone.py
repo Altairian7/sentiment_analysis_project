21a271955ac00ab1405ead044198784947a3ab7f
altairian7 <artisharma.as12.as@gmail.com>
2025-05-09 22:50:11 +0530

    record voice func

--- voice_cloning/clone.py
+++ voice_cloning/clone.py
@@ -1,3 +1,16 @@
 pip install TTS
 pip install resemblyzer
 pip install pydub sounddevice scipy
+
+
+import sounddevice as sd
+from scipy.io.wavfile import write
+
+def record_voice(filename="voice_sample.wav", duration=10, fs=16000):
+    print(f"Recording for {duration} seconds...")
+    audio = sd.rec(int(duration * fs), samplerate=fs, channels=1, dtype='int16')
+    sd.wait()
+    write(filename, fs, audio)
+    print(f"Saved to {filename}")
+
+record_voice()
